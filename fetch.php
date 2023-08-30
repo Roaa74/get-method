ed020443591666960da5f1cb8e35288f34e9c386
Roaa74 <140811326+Roaa74@users.noreply.github.com>
2023-08-30 14:24:18 +0300

    Update fetch.php

--- fetch.php
+++ fetch.php
@@ -1,1 +1,28 @@
-.
+<?php
+// connect to database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "getmethod";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("connection failed: " . $conn->connect_error);
+}
+
+// get search query from GET request
+$search = $_GET['search'];
+
+// search database and display results
+$sql = "SELECT * FROM numbers WHERE input = '$search'";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "ID: " . $row["ID"] . " - Input: " . $row["input"] . "<br>";
+    }
+} else {
+    echo "No results found";
+}
+
+// close database connection
+$conn->close();
+?>te
