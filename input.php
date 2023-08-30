a873ec5729b9cbb94bf09d9cab48de525c494e08
Roaa74 <140811326+Roaa74@users.noreply.github.com>
2023-08-30 14:23:58 +0300

    Update input.php

--- input.php
+++ input.php
@@ -1,1 +1,46 @@
-xd
+
+<?php
+
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "getmethod";
+
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+
+    if ($conn->connect_error) {
+        die("connection field" . $conn->connect_error);
+    }
+
+    $number = $_GET['number'];
+    $sql = "INSERT INTO numbers (input) VALUES ('$number')";
+
+
+
+    if ($conn->query($sql) === TRUE) {
+        echo "<br>";
+    } else {
+        echo "an error happened" . $conn->error;
+    }
+
+    $sql = "SELECT * FROM numbers WHERE id=(SELECT MAX(id)FROM numbers)";
+
+
+$result = $conn->query($sql);
+
+
+if ($result->num_rows > 0) {
+
+    while($row = $result->fetch_assoc()) {
+        echo "num: "   . $_GET["number"] ;echo " <br>ID : " . $row["ID"];
+    }
+} else {
+    echo "there is no data";
+}
+
+    $conn->close();
+
+?>
